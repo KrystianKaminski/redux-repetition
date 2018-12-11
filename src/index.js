d369346b7f5bde87ed00f39ae9741db3803d32b1
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-11 09:52:03 +0100

    store.js created

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, combineReducers } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = combineReducers({
+    // reducers
+})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+)
