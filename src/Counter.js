5a1331f92ca49149c954f2ee45b9dea8756bc7f3
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-11 11:54:39 +0100

    Counter component created

---
+++ src/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Counter = () => (
+    <div>
+        <h1>Value: </h1>
+        <button>+</button>
+        <button>-</button>
+    </div>
+
+)
+
+export default Counter
--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import { store } from './store'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { Provider } from 'react-redux'
 
+ReactDOM.render(<App/>, document.getElementById('root'))
