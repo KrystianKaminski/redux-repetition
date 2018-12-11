4c2be4c83b8a41f6e6690e6399f2b1069568fd25
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-11 11:41:00 +0100

    Example with counter

--- src/store.js
+++ src/store.js
@@ -4,6 +4,13 @@ const INITIAL_STATE = {
     firstAction: false
 }
 
+const COUNTER_INITIAL_STATE = {
+    counter: 0
+}
+
+const INC = 'counter/INC'
+const DEC = 'counter/DEC'
+
 const reducer = (state = INITIAL_STATE, action) => {
     if (action.type === 'FIRST_ACTION') {
         return {
@@ -14,7 +21,21 @@ const reducer = (state = INITIAL_STATE, action) => {
     return state // by default returns prev state
 }
 
-const counterReducer = () => ({counter: 0})
+const counterReducer = (state = COUNTER_INITIAL_STATE, action) => {
+    if (action.type === INC) {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+    if (action.type === DEC) {
+        return {
+            ...state,
+            counter: state.counter - 1
+        }
+    }
+    return state
+}
 
 const reducersCombine = combineReducers({
     firstActionReducerName: reducer,
@@ -31,9 +52,20 @@ const FIRST_ACTION = 'FIRST_ACTION'
 
 const firstAction = () => ({ type: FIRST_ACTION})
 
+const add = () => ({type: INC})
+const dec = () => ({type: DEC})
+
 window.dispatchFirstAction = () => store.dispatch(firstAction())
+
+window.increment = () => store.dispatch(add())
+window.decrement = () => store.dispatch(dec())
 
 window.dispatchFirstAction()
 window.dispatchFirstAction()
 window.dispatchFirstAction()
 window.dispatchFirstAction()
+
+window.increment()
+window.increment()
+window.increment()
+window.decrement()
